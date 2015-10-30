9a2690bc778aa97935c849969fb2e9dbc646c3e2
Mike Cooper <shivagogo@gmail.com>
2015-10-30 09:48:36 -0400

    Submitting

--- rover.rb
+++ rover.rb
@@ -1,8 +1,78 @@
 class Rover
-
+	#attr_accessor :x_coordinate, :y_coordinate, :direction
 	def initialize(x_coordinate, y_coordinate, direction)
 		@x_coordinate = x_coordinate
 		@y_coordinate = y_coordinate
 		@direction = direction
 	end
+
+	def read_instruction
+		if
+
+		elsif
+
+		else
+
+	end
+
+
+	def move
+		if
+
+		elsif
+
+		else
+
+	end
+
+
+	def turn
+		if
+
+		elsif
+
+		else
+
+	end
+
 end
+
+#split/parse coordinates to rover
+
+position = gets.chomp
+x_coordinate = gets.chomp
+y_coordinate = gets.chomp
+direction = gets.chomp
+
+# L = -1
+# R = 1
+# M = 1
+
+
+	def output
+		puts "#{x_coordinate} #{y_coordinate} #{direction}"
+	end
+
+
+
+
+#+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
